fec96dc92530c8971d087d0503a843554cd5273e
Hayatunnis69 <atunhayatun49@gmail.com>
2023-06-05 18:51:50 +0700

    upload praktikum

--- praktikum6/dbkoneksi.php
+++ praktikum6/dbkoneksi.php
@@ -1,16 +1,19 @@
-<?php
-    $host = 'localhost:3307';
-    $dbname = 'dbpos';
-    $username = 'root';
-    $password = '';
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<?php 
+  $host = 'localhost';
+  $db = 'dbpos';
+  $user = 'root';
+  $pass = '';
+  $charset='utf8mb4';
 
+  $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
-    try{
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $e) {
-        echo "Koneksi Gagal : " . $e->getMessage();
-    }
+  $opt = [
+    PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES=>false,
+  ];
 
+  $dbh =  new PDO($dsn,$user,$pass,$opt);
 
 ?>
--- praktikum7/class_bmi.php
+++ praktikum7/class_bmi.php
@@ -1,43 +1,50 @@
-<?php 
-// Class BMIPASIEN
-class bmiPasien{
-    //Bikin propoty
+<?php
+// Bikin class BMI
+class bmi {
+    // Membuat Property
     public $nama, $berat, $tinggi, $umur, $jk;
 
-    //Construct : data nya diset sesuai user
+    // Construct data nya diset sesuai user
     function __construct($nama, $berat, $tinggi, $umur, $jk){
+        // Masukan data
         $this->nama = $nama;
         $this->berat = $berat;
         $this->tinggi = $tinggi;
         $this->umur = $umur;
         $this->jk = $jk;
-
     }
 
-    //bikin method
+    // Membuat method
     public function hasilBMI(){
+        // Login
         $tinggi_m = $this->tinggi / 100;
-        //rumus bmi
+        // Rumus BMI
         $bmi = $this->berat / ($tinggi_m * $tinggi_m);
         return round($bmi);
+
     }
 
-    // method
+    // Membuat method
     public function statusBMI(){
-        //simpan data bmi
+        // Simpan data bmi
         $bmi = $this->hasilBMI();
-        //cek data menjadi status
+        // cek data menjadi status
         if ($bmi < 18.5){
             return "Kekurangan Berat Badan";
-        } elseif ($bmi >= 18.5 && $bmi <= 24.9){
-            return "Normal (Ideal)";
-        } elseif ($bmi >=25 && $bmi <=29) {
+        }elseif ($bmi >= 18.5 && $bmi <= 24.9){
+            return "Normal (ideal)";
+        }elseif ($bmi >= 25 && $bmi <= 29){
             return "Kelebihan Berat Badan";
-        } else {
+        }else {
             return "Obesitas";
         }
 
     }
 }
 
+// Bikin Objek
+// $pasien = new bmipasien("Udin", "59", "174", "19", "L");
+// echo "Nama :" . $pasien->nama . "<br>";
+// echo "BMI" :" . $pasien->hasilBMI() . "<br>";
+// echo "Status BMI :" . $pasien->statusBMI() . "<br>"
 ?>
